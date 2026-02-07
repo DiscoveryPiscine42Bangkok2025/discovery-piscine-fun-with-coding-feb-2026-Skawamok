1c4ef9413f41c9406a535384088fe0ebcb686a7d
Skawamok <skawa881236@gmail.com>
2026-02-07 14:40:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 async function quickLogin(selectedUser) {
     const side = (selectedUser === 'tathanad') ? 'left' : 'right';
     const errorElement = document.getElementById(`error-${side}`);
+    
     const redirectUrl = `${selectedUser}/index.html`; 
 
     try {
         const response = await fetch('auteur');
-        if (!response.ok) throw new Error("Cannot read auteur file");
+        
+        if (!response.ok) {
+            errorElement.innerText = "System Error: Missing file";
+            return; 
+        }
         
         const text = await response.text();
         const validUsers = text.split('\n').map(u => u.trim());
 
         if (validUsers.includes(selectedUser)) {
-            
-            errorElement.style.color = "#2ecc71";
+            errorElement.style.color = "#2ecc71"; 
             errorElement.innerText = "Redirecting...";
             
             setTimeout(() => {
@@ -20,16 +24,11 @@ async function quickLogin(selectedUser) {
             }, 500);
             
         } else {
-            errorElement.style.color = "red";
-            errorElement.innerText = "Error: User not found in auteur!";
+            errorElement.style.color = "#ff4757"; 
+            errorElement.innerText = "Access Denied!";
         }
 
     } catch (error) {
-        console.error(error);
         errorElement.innerText = "System Error";
     }
-}
-
-function resetForm(side) {
-    document.getElementById(`error-${side}`).innerText = "";
 }
